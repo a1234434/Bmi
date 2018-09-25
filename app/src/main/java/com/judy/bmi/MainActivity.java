0aa736de1f6960b4ad77b9cf4c4b47d04acdef34
Judy <aju452488@gmail.com>
2018-09-25 15:31:48 +0800

    first commit

---
+++ app/src/main/java/com/judy/bmi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.judy.bmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
